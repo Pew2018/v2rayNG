efb3ad5ffeb66f4955b81f1937b34ab2eec96c7c
Patrick <94493866+Pew2018@users.noreply.github.com>
2026-08-28 12:53:59 +0800

    fix: import Color for Pixel tab indicator

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/main/MainGroupTab.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/main/MainGroupTab.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
